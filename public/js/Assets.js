2f2c1c98d120f8a85e8d553b5cc62b30fe2f5c55
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-02-25 18:35:58 +0900

    fix

--- public/js/Assets.js
+++ public/js/Assets.js
@@ -6,7 +6,7 @@ class Assets
     {
         //background image
         this.imageField = new Image()
-        this.imageField.src = './images/grass.png'
+        this.imageField.src = '../images/grass.png'
 
         this.rectFieldInFieldImage = {sx: 0, sy: 0, sw: 512, sh: 512};
     }
