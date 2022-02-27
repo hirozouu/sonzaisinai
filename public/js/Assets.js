b4e0209c60b7f9341e9f48c644c1e2a7d4e7b135
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-02-27 16:14:46 +0900

    fix

--- public/js/Assets.js
+++ public/js/Assets.js
@@ -11,10 +11,10 @@ class Assets
         this.rectFieldInFieldImage = {sx: 0, sy: 0, sw: 512, sh: 512};
 
         // item image
-        this.imahgeItems = new Image()
-        this.imahgeItems.src = '../images/items.png'
+        this.imageItems = new Image()
+        this.imageItems.src = '../images/items.png'
         
-        this.arectTankInItemImage = [
+        this.arectTankInItemsImage = [
             {sx: 2, sy: 2, sw: 16, sh: 16}, 
             {sx: 20, sy: 2, sw: 16, sh: 16}, 
         ];
