1faffffb609c13ef975abaf0504379c614983217
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-02-27 14:55:46 +0900

    add

--- libs/GameSetting.js
+++ libs/GameSetting.js
@@ -3,4 +3,6 @@
 module.exports = class GameSettings
 {
     static get FRAMERATE() {return 30;} //framerate
+    static get TANK_SPEED() {return 150.0;} //speed
+    static get TANK_ROTATION_SPEED() {return 3.0;} //rotation speed
 }
