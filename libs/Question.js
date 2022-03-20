c32e59694972253866fd387b475c62e314497e6d
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-03-20 16:49:23 +0900

    fix

--- libs/Question.js
+++ libs/Question.js
@@ -32,13 +32,14 @@ module.exports = class Question
         function fisherYatesShuffle(arr){
             for(var i = arr.length-1; i>0; i--){
                 var j = Math.floor(Math.random() * (i + 1)); //random index
-                [arr[i],arr[j]]=[arr[j],arr[i]]; // swap
+                [arr[i],arr[j]]=[arr[j], arr[i]]; // swap
             }
 
             return arr;
         }
 
         shuffle_answers = fisherYatesShuffle(this.answers);
+        console.log(shuffle_answers[0])
         return shuffle_answers;
     }
 }
