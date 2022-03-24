8e62716bcfafc1a1aa24da3451f7e5e304ac0160
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-03-24 17:38:42 +0900

    test

--- libs/Game.js
+++ libs/Game.js
@@ -77,10 +77,7 @@ module.exports = class Game
                 socket.on("get-ready", 
                 (json) =>
                     {
-                        console.log("hogehoge")
-                        console.log("%s", json.roomName)
-                        console.log("hogehoge")
-                        socket.broadcast.to(json.roomName).emit("get-ready", json);
+                        io.sockets.in(json.roomName).emit("get-ready", json);
                     }
                 );
 
