934a6a41916dcbb2e8e289a63b68d33fe92bb2d4
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-03-24 18:03:09 +0900

    test

--- libs/Game.js
+++ libs/Game.js
@@ -61,7 +61,7 @@ module.exports = class Game
                 socket.on("enter-the-room", 
                 (json) =>
                     {
-                        socket.to(json.roomName).emit("enter-the-room", json);
+                        socket.to("hoge").emit("enter-the-room", json);
                     }
                 );
 
