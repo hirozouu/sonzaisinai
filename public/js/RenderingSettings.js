a239a7fae2012880b8a6b1afaf9cd98ce7e34509
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-03-28 22:02:48 +0900

    add change color

--- public/js/RenderingSettings.js
+++ public/js/RenderingSettings.js
@@ -5,14 +5,13 @@ class RenderingSettings
 
 }
 
-document
-
 function clickSelection(element, num) {
-    document.getElementById("select1").class = "selection";
-    document.getElementById("select2").class = "selection";
-    document.getElementById("select3").class = "selection";
-    document.getElementById("select4").class = "selection";
-    element.class = "selection_selected";
+
+    document.getElementById("select1").children('p').style.color = 'black';
+    document.getElementById("select2").children('p').style.color = 'black';
+    document.getElementById("select3").children('p').style.color = 'black';
+    document.getElementById("select4").children('p').style.color = 'black';
+    this.children('p').style.color = "red";
     console.log("click : selection%s", num)
     SELECT = num;
 }
