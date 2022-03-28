ea4838f686b21eb145ac065a3eb244706d34de8c
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-03-28 22:32:53 +0900

    fix

--- public/js/RenderingSettings.js
+++ public/js/RenderingSettings.js
@@ -6,11 +6,12 @@ class RenderingSettings
 }
 
 function clickSelection(element, num) {
-    $("#select1 > p").css("color", "black")
-    $("#select2 > p").css("color", "black")
-    $("#select3 > p").css("color", "black")
-    $("#select4 > p").css("color", "black")
-    $("#select${num} > p").css('color', "red")
+    arr = ["NO_NAME", "#select1 > p", "#select2 > p", "#select3 > p", "#select4 > p"];
+    $("#select1 > p").css("color", "black");
+    $("#select2 > p").css("color", "black");
+    $("#select3 > p").css("color", "black");
+    $("#select4 > p").css("color", "black");
+    $(arr[num]).css("color", "red");
     console.log("click : selection%s", num)
     SELECT = num;
 }
