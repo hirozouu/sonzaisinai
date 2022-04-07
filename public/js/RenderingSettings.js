18b486300542eab2888976116fe2bb1a573637d9
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-04-07 19:23:33 +0900

    fix

--- public/js/RenderingSettings.js
+++ public/js/RenderingSettings.js
@@ -7,7 +7,7 @@ class RenderingSettings
 
 function clickSelection(element, num) {
     const arr_p = ["#select1 > p", "#select2 > p", "#select3 > p", "#select4 > p"];
-    const arr_div = ["#select1", "select2", "select3", "select4"]
+    const arr_div = ["#select1", "#select2", "#select3", "#select4"]
     for (var ref of arr_p)
     {
         $(ref).css("color", "white");
