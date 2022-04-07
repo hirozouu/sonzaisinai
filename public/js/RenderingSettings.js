766eb4ae42cf7a89a7c446bc8e9ee63bbdfe6e91
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-04-07 19:21:14 +0900

    add selection

--- public/js/RenderingSettings.js
+++ public/js/RenderingSettings.js
@@ -6,12 +6,18 @@ class RenderingSettings
 }
 
 function clickSelection(element, num) {
-    var arr = ["#select1 > p", "#select2 > p", "#select3 > p", "#select4 > p"];
-    $("#select1 > p").css("color", "black");
-    $("#select2 > p").css("color", "black");
-    $("#select3 > p").css("color", "black");
-    $("#select4 > p").css("color", "black");
-    $(arr[num]).css("color", "red");
+    const arr_p = ["#select1 > p", "#select2 > p", "#select3 > p", "#select4 > p"];
+    const arr_div = ["#select1", "select2", "select3", "select4"]
+    for (var ref of arr_p)
+    {
+        $(ref).css("color", "white");
+    }
+    for (var id of arr_div)
+    {
+        $(id).css("color", "#5342eb")
+    }
+    $(arr_p[num]).css("color", "#5342eb");
+    $(arr_div[num]).css("background", "white")
     console.log("click : selection%s", num)
     SELECT = num;
 }
