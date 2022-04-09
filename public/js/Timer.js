56740e1fa36ce8b72cd5f5458799b4cacc4b989f
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-04-09 19:32:57 +0900

    fix

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -11,6 +11,11 @@ class Timer
     {
         var setTime = 10;
         $(".circle").removeClass("pie");
+        $(".circle").css(
+            {
+                "stroke-dasharray": "0,157"
+            }
+        );
         $(".circle")[0].offsetWidth = $(".circle")[0].offsetWidth;
         $(".circle").addClass("pie");
         $(".circle").css(
