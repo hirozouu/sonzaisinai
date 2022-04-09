53db43d408eb46d4ef5d0c52abf8b351485f97ba
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-04-09 21:03:20 +0900

    add timer

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -9,6 +9,23 @@ class Timer
 
     setTimer()
     {
-        ;
+        var setTime = 10;   //60秒に設定
+    var second = 0;
+    const circle = document.querySelector(".circle");
+    const seconds = document.querySelector(".second");
+
+    circle.classList.add('pie');
+    circle.style.animation =  'pie '+ setTime*2 +'s linear';
+        var timerId = setInterval(function() {
+            second += 1;
+            if(second >= setTime){
+                clearInterval(timerId);
+            }
+            countTime = setTime - second;
+            seconds.textContent= (countTime % 60);
+            if(countTime < 0){  //10秒切ったら文字が赤くなります
+                document.querySelector(".seconds").style.color = 'red';
+            };
+        }, 1000);
     }
 }
