40f6bebd4c27790f1d3153d61b654b6d26f57f03
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-04-09 18:20:46 +0900

    fix

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -10,21 +10,8 @@ class Timer
     setTimer()
     {
         var setTime = 10;
-        var second = 0;
-        $('.circle').addClass('pie');
-        $('.circle').css({'animation': 'pie '+ setTime*2 +'s linear'});
-        var timerId = setInterval(function() 
-        {
-            second += 1;
-            if(second >= setTime)
-            {
-                clearInterval(timerId);
-            }
-        }, 1000);
-    }
-
-    resetTimer()
-    {
-        $(".circle").removeClass('pie');
+        $(".circle").removeClass("pie");
+        $(".circle").addClass("pie");
+        $(".circle").css({"animation": "pie "+ setTime*2 +"s linear"});
     }
 }
--- public/js/client.js
+++ public/js/client.js
@@ -118,7 +118,6 @@ socket.on("set-question",
         document.getElementById("answer").style.display = "none";
         screen.renderQuestion(json);
         document.getElementById("timer").style.visibility = "visible";
-        timer.resetTimer();
         timer.setTimer();
         timecount = setTimeout(function()
         {
