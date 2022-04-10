c71d5a6d7285bf856a84d56be8025b23380f4a47
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-04-10 15:05:57 +0900

    fix

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -15,7 +15,6 @@ class Timer
     const circle = document.querySelector(".circle");
     const seconds = document.querySelector(".second");
 
-    circle.classList.remove('pie');
     circle.classList.add('pie');
     circle.style.animation =  'pie '+ setTime*2 +'s linear';
         var timerId = setInterval(function() {
@@ -33,7 +32,15 @@ class Timer
 
     resetTimer()
     {
-        document.querySelector(".progressCircle").getClientRects();
+        document.querySelector(".circle").className = "circle";
+        window.requestAnimationFrame(function(time)
+        {
+            window.requestAnimationFrame(function(time)
+            {
+                document.querySelector(".circle").className = "circle pie"
+            });
+        });
+
         // var elem = document.querySelector(".progressCircle");
         // var p = elem.parentElement;
         // var dummy = document.createElement("div");
