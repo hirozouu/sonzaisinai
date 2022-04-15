d52b25124b014932ca8a131ae273a05a7f38bf62
hirozouu <83117195+hirozouu@users.noreply.github.com>
2022-04-15 14:58:17 +0900

    change color

--- public/js/RenderingSettings.js
+++ public/js/RenderingSettings.js
@@ -18,9 +18,9 @@ function clickSelection(element, num)
         $(id).css("background", "#50c1e9")
         $(id).css("border-top", "solid 1px #48add1")
     }
-    $(arr_p[num]).css("color", "dcf2fa");
-    $(arr_div[num]).css("background", "#409aba")
-    $(arr_div[num]).css("border-top", "solid 5px #48add1")
+    $(arr_p[num]).css("color", "#dcf2fa");
+    $(arr_div[num]).css("background", "#48add1")
+    $(arr_div[num]).css("border-top", "solid 5px #409aba")
     console.log("click : selection%s", num)
     SELECT = num;
 }
